16e22b4b3376491b2efa85738894d1f062254de3
lindakw <linkda01@icloud.com>
2022-03-21 11:56:01 -0700

    added back end

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 3001;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api", require("./routes/api-routes"));
+
+app.listen(PORT, () => {
+  if (process.env.NEWS_API === undefined) {
+    throw new Error(
+      `Create a .env file with 'NEWS_API=<"YOUR_API_KEY"> from newsapi.org`
+    );
+  }
+  console.log(`Listening on port ${PORT}`);
+});
